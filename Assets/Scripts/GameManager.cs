391ab787d19a6a777e43b396d5846d1e895e6124
ksurdyk1 <karsur121@wp.pl>
2022-12-10 01:52:13 +0100

    added boundaries - player cannot go out of the screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,10 +24,24 @@ public class GameManager : MonoBehaviour
     {
         var moveDirectionLeft = moveActionLeft.ReadValue<Vector2>();
         var moveDirectionRight = moveActionRight.ReadValue<Vector2>();
-
-        positionLeft += moveDirectionLeft * moveSpeed * Time.deltaTime;
-        positionRight += moveDirectionRight * moveSpeed * Time.deltaTime;
-
+        Vector2 newPositionLeft = positionLeft + moveDirectionLeft * moveSpeed * Time.deltaTime;
+        Vector2 newPositionRight = positionRight + moveDirectionRight * moveSpeed * Time.deltaTime;
+        if (newPositionLeft.x > -Screen.width / 2 && newPositionLeft.x < Screen.width/2)
+        {
+            positionLeft.x = newPositionLeft.x;
+            if (newPositionLeft.y < Screen.height / 2 && newPositionLeft.y > -Screen.height / 2)
+            {
+                positionLeft.y = newPositionLeft.y;
+            }
+        }
+        if (newPositionRight.x > -Screen.width / 2 && newPositionRight.x < Screen.width/2)
+        {
+            positionLeft.x = newPositionRight.x;
+            if (newPositionRight.y < Screen.height / 2 && newPositionRight.y > -Screen.height / 2)
+            {
+                positionLeft.y = newPositionRight.y;
+            }
+        }
         player1.position = new Vector3(positionLeft.x,positionLeft.y, player1.position.z);
         player2.position =  new Vector3(positionRight.x,positionRight.y, player2.position.z);
     }
