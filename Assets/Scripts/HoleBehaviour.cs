b7d3b3bda83d9436a20d7c8df1f3cd3c13ef00e6
ksurdyk1 <karsur121@wp.pl>
2022-12-10 03:07:49 +0100

    added down hole behaviour

--- Assets/Scripts/HoleBehaviour.cs
+++ Assets/Scripts/HoleBehaviour.cs
@@ -7,8 +7,14 @@ public class HoleBehaviour : MonoBehaviour
 {
     [SerializeField] private EnemyBehaviour enemyPrefab;
     [Range(0, 500)]
-    public int forceRange = 200;
-
+    [SerializeField] private int forceRange = 200;
+    [Range(50, 500)]
+    [SerializeField] private int forceRangeY = 320;
+    [Range(0, 100)]
+    [SerializeField] private int forceRangeDownX = 50;
+    [Range(50, 500)]
+    [SerializeField] private int minForceDownY = 50;
+    [SerializeField] private bool isDown = false;
     [SerializeField] private float cooldown = 3f;
 
     
@@ -21,8 +27,15 @@ public class HoleBehaviour : MonoBehaviour
     {
         var enemy = Instantiate(enemyPrefab, transform.position, quaternion.identity);
         enemy.transform.SetParent(transform);
-        var rand = Random.Range(-forceRange, forceRange);
-        enemy.GetComponent<Rigidbody2D>().AddForce(new Vector2(rand,0));
+        int rand = Random.Range(-forceRange, forceRange);
+        int randY = 0;
+        if (isDown)
+        {
+            rand = Random.Range(-forceRangeDownX, forceRangeDownX);
+            randY = Random.Range(minForceDownY, forceRangeY);
+        }
+        
+        enemy.GetComponent<Rigidbody2D>().AddForce(new Vector2(rand,randY));
         yield return new WaitForSeconds(cooldown);
         StartCoroutine(SpawnEnemy());
 
