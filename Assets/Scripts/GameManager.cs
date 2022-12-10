bd7f90307456b74dde9625965eb99070454aebb0
necr0o0 <radekklaser@gmail.com>
2022-12-10 02:12:31 +0100

    added players boundries

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -11,7 +12,6 @@ public class GameManager : MonoBehaviour
     public Vector2 positionRight;
     public Transform player1;
     public Transform player2;
-
     void Start()
     {
         moveActionLeft.Enable();
@@ -19,30 +19,36 @@ public class GameManager : MonoBehaviour
 
     }
 
-   
-    void Update()
+    private void MovePlayers()
     {
         var moveDirectionLeft = moveActionLeft.ReadValue<Vector2>();
         var moveDirectionRight = moveActionRight.ReadValue<Vector2>();
+        
         Vector2 newPositionLeft = positionLeft + moveDirectionLeft * moveSpeed * Time.deltaTime;
         Vector2 newPositionRight = positionRight + moveDirectionRight * moveSpeed * Time.deltaTime;
-        if (newPositionLeft.x > -Screen.width / 2 && newPositionLeft.x < Screen.width/2)
+        
+        if (newPositionLeft.x > -8.9 && newPositionLeft.x < 8.9)
         {
             positionLeft.x = newPositionLeft.x;
-            if (newPositionLeft.y < Screen.height / 2 && newPositionLeft.y > -Screen.height / 2)
+            if (newPositionLeft.y < 4 && newPositionLeft.y > -6)
             {
                 positionLeft.y = newPositionLeft.y;
             }
         }
-        if (newPositionRight.x > -Screen.width / 2 && newPositionRight.x < Screen.width/2)
+        if (newPositionRight.x > -8.9 && newPositionRight.x < 8.9)
         {
-            positionLeft.x = newPositionRight.x;
-            if (newPositionRight.y < Screen.height / 2 && newPositionRight.y > -Screen.height / 2)
+            positionRight.x = newPositionRight.x;
+            if (newPositionRight.y < 4 && newPositionRight.y > -6)
             {
-                positionLeft.y = newPositionRight.y;
+                positionRight.y = newPositionRight.y;
             }
         }
         player1.position = new Vector3(positionLeft.x,positionLeft.y, player1.position.z);
         player2.position =  new Vector3(positionRight.x,positionRight.y, player2.position.z);
+    }
+   
+    void Update()
+    {
+        MovePlayers();
     }
 }
